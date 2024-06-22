998df87a6fe9bf2000f647ec8643ae789e1c4a33
Legitrue <channborry@gmail.com>
2024-06-22 19:08:20 +0300

    Добавил ссылки справа и бункер уебал влева

--- includes/header.php
+++ includes/header.php
@@ -3,4 +3,10 @@
         <b>
             БУНКЕР
         </b>
+                <ul class="nav-links">
+                    <li><a href="#home">Главная</a></li>
+                    <li><a href="#rules">Правила игры</a></li>
+                    <li><a href="#wiki">Вики</a></li>
+                    <li><a href="#updates">Обновления</a></li>
+                </ul>
 </div>
