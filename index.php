62dd378913aa9ab21d402c349ae8ff45427d3033
Destructzz <sklyarenko_2006@mail.ru>
2024-06-23 20:17:01 +0300

    fix 404

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+// Получение запрашиваемого URI
+$request = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+// Получение полного пути к файлу
+$file = $_SERVER['DOCUMENT_ROOT'] . $request;
+
+// Проверка существования файла или директории
+if ($request !== '/' && (!file_exists($file) || is_dir($file))) {
+    header("HTTP/1.0 404 Not Found");
+    include '404.php';
+    exit();
+}
+?>
--- menu.php
+++ menu.php
@@ -1,17 +1,3 @@
-<?php
-// Получение запрашиваемого URI
-$request = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
-// Получение полного пути к файлу
-$file = $_SERVER['DOCUMENT_ROOT'] . $request;
-
-// Проверка существования файла или директории
-if ($request !== '/' && (!file_exists($file) || is_dir($file))) {
-    header("HTTP/1.0 404 Not Found");
-    include '404.php';
-    exit();
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
